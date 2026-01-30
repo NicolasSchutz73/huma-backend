334cadab0f546ba6e3a11566c72908307210b08e
nicolasschutz <pro.nicolas.schutz@gmail.com>
2026-01-30 09:55:48 +0100

    feature : add swagger

--- src/routes/feedbacks.js
+++ src/routes/feedbacks.js
@@ -6,9 +6,74 @@ const validate = require('../middleware/validate');
 const feedbackSchemas = require('../validators/feedbackSchemas');
 
 // GET /api/feedbacks - Lister l'historique des feedbacks
+/**
+ * @swagger
+ * /feedbacks:
+ *   get:
+ *     tags: [Feedbacks]
+ *     summary: List feedback history
+ *     security:
+ *       - UserIdHeader: []
+ *     parameters:
+ *       - in: header
+ *         name: X-User-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User identifier
+ *     responses:
+ *       200:
+ *         description: Feedback list
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.get('/', authenticate, feedbackController.getFeedbacks);
 
 // POST /api/feedbacks - Créer un nouveau ticket
+/**
+ * @swagger
+ * /feedbacks:
+ *   post:
+ *     tags: [Feedbacks]
+ *     summary: Create a feedback ticket
+ *     security:
+ *       - UserIdHeader: []
+ *     parameters:
+ *       - in: header
+ *         name: X-User-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User identifier
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               category:
+ *                 type: string
+ *                 enum: [WORKLOAD, RELATIONS, MOTIVATION, ORGANIZATION, RECOGNITION, WORK_LIFE_BALANCE, FACILITIES]
+ *               feedbackText:
+ *                 type: string
+ *               solutionText:
+ *                 type: string
+ *               isAnonymous:
+ *                 type: boolean
+ *             required: [category, feedbackText, solutionText]
+ *     responses:
+ *       200:
+ *         description: Feedback created
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.post('/', authenticate, validate(feedbackSchemas.createFeedback), feedbackController.createFeedback);
 
 console.log('Loading feedbacks routes...');
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -8,9 +8,123 @@ const userSchemas = require('../validators/userSchemas');
 console.log('Loading users routes...');
 
 // Toutes les routes de ce fichier sont protégées par authenticate
+/**
+ * @swagger
+ * /users/me:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get current user info
+ *     security:
+ *       - UserIdHeader: []
+ *     parameters:
+ *       - in: header
+ *         name: X-User-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User identifier
+ *     responses:
+ *       200:
+ *         description: User info
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.get('/me', authenticate, userController.getUserInfo);
+
+/**
+ * @swagger
+ * /users/me/onboarding:
+ *   put:
+ *     tags: [Users]
+ *     summary: Complete user onboarding
+ *     security:
+ *       - UserIdHeader: []
+ *     parameters:
+ *       - in: header
+ *         name: X-User-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User identifier
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               work_style:
+ *                 type: string
+ *               motivation_type:
+ *                 type: string
+ *               stress_source:
+ *                 type: string
+ *             required: [work_style, motivation_type, stress_source]
+ *     responses:
+ *       200:
+ *         description: Onboarding completed
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.put('/me/onboarding', authenticate, validate(userSchemas.completeOnboarding), userController.completeOnboarding);
+
+/**
+ * @swagger
+ * /users/me/test:
+ *   put:
+ *     tags: [Users]
+ *     summary: Test endpoint
+ *     responses:
+ *       200:
+ *         description: Test response
+ *       500:
+ *         description: Server error
+ */
 router.put('/me/test', (req, res) => res.json({msg: "Test OK"}));
+
+/**
+ * @swagger
+ * /users/me/info:
+ *   put:
+ *     tags: [Users]
+ *     summary: Update user information
+ *     security:
+ *       - UserIdHeader: []
+ *     parameters:
+ *       - in: header
+ *         name: X-User-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User identifier
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               first_name:
+ *                 type: string
+ *               last_name:
+ *                 type: string
+ *             required: [first_name, last_name]
+ *     responses:
+ *       200:
+ *         description: User updated
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.put('/me/info', authenticate, validate(userSchemas.updateUserInfo), userController.updateUserInfo);
 
 console.log('Router stack size:', router.stack.length);
